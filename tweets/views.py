81ba5f657fbac79e70d906140cd12140c5d7ec49
Neo945 <shreeshsrvstv@gmail.com>
2021-02-09 21:33:32 +0530

    REST API view for tweet details

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http.response import Http404
+from django.http.response import Http404, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import HttpResponse
@@ -7,14 +7,21 @@ from .models import Tweet
 # Create your views here.
 class HomeView(View):
     def get(self,request,*args,**kwargs):
-        print(request,args,kwargs)
+        # print(request,args,kwargs)
         return HttpResponse("Hello World")
 
 class Tweets_detail_views(View):
     def get(self,request,tweet_id,*args,**kwargs):
+        data = {
+            "id" : int(tweet_id),
+        }
+        status = 200
         try:
-            tweet = Tweet.objects.get(pk = tweet_id)
+            tweet = Tweet.objects.get(pk = int(tweet_id))
+            data['content'] = tweet.content
         except:
-            raise Http404
-        return HttpResponse(f"Hello {tweet_id}- {tweet.content}")
+            # raise Http404
+            data['message'] = 'Not found'
+            status = 404
+        return JsonResponse(data,status=status)
 
