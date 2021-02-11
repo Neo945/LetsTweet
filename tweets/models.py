c61ceb5b6fc27e622a00310202dc7e9b1beaa8d2
Neo945 <shreeshsrvstv@gmail.com>
2021-02-11 18:00:48 +0530

    Added the meta class(nested class) for ordering the data in descending order Added the ability to push the element on the top of List

--- tweets/models.py
+++ tweets/models.py
@@ -5,6 +5,9 @@ class Tweet(models.Model):
     content = models.TextField(null=True,blank=True)
     image = models.FileField(upload_to='images/',blank=True,null=True)
 
+    class Meta:
+        ordering = ['-id']
+
     def serialize(self):
         return {
             "id":self.id,
