40cad0ef7783d8773b95e7938c4510632d67d9ff
Neo945 <shreeshsrvstv@gmail.com>
2021-02-16 22:03:17 +0530

    Like and dislike properties added in the tweet-web

--- tweet-web/src/App.js
+++ tweet-web/src/App.js
@@ -19,14 +19,29 @@ function Loadtweets(callBack) {
 
 function Tweet(props){
   return (
-    <div className='col-12 border py-3 mb-4 tweet' id={`tweet-${props.tweet.id}`}>
-        <p>{props.tweet.content}</p></div>
+    <div className='col-12 border py-3 mb-4 tweet' id={`t-${props.tweet.id}`}>
+        <p>{props.tweet.id}. {props.tweet.content}</p></div>
   )
 }
+const getCaps = (str) => str.charAt(0).toUpperCase() + str.slice(1)
 
 function ActionButton(props) {
+  let display = getCaps(props.action)
+  const [likes,setLikes] = useState(props.tweet.likes)
+  const [userLiked,setDidUserLiked] = useState(props.tweet.likes!==0)
+  const handleAction = (event)=> {
+    // console.log(props.tweet.id)
+    if(userLiked){
+      setLikes(likes - 1)
+      setDidUserLiked(false)  
+    }else{
+      setLikes(likes + 1)
+      setDidUserLiked(true)  
+    }
+  }
+  display = props.action==='like'? (likes===0 ? display :  likes + ' ' + display) : display
   return (
-    <button class='btn btn-outline-primary btn-sm'>{props.tweet.likes} LIKE</button>
+    <button className='btn btn-outline-primary btn-sm' onClick={handleAction}>{display}</button>
   )
 }
 
@@ -47,10 +62,11 @@ function App() {
   return (
     <div className="root">
         {tweet.map(
-          (tweet,index)=>{
+          (tweet)=>{
             return (<div>
-              <Tweet tweet={tweet} key={index}/>
-              <ActionButton tweet={tweet} key={index}/>
+              <Tweet tweet={tweet} key={`tweet-${tweet.id}`}/>
+              <ActionButton tweet={tweet} key={`like-${tweet.id}`} action='like'/>
+              <ActionButton tweet={tweet} key={`retweet-${tweet.id}`} action='retweet'/>
             </div>)
         }
           )}
