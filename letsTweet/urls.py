4eca47773b9d67210d75b93e2e1b5ef3f1012554
Neo945 <shreeshsrvstv@gmail.com>
2021-02-25 17:07:04 +0530

    Added the production data into the static files and rendering those files from Django views

--- letsTweet/urls.py
+++ letsTweet/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from tweets.views import HomeView,Tweets_detail_views,tweet_list_view,TweetForm,Tweets_delete_views,Tweets_action_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +28,9 @@ urlpatterns = [
     path('tweets_action', Tweets_action_views),
     path('tweets/<int:tweet_id>', Tweets_detail_views),
     path('create-tweet',TweetForm),
+    path('react/',TemplateView.as_view(template_name='react.html'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
